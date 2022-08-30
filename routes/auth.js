b0539f32ad4cc417c2f90214edca78ec9cd4376f
Ramazan Alp <rmznalp@gmail.com>
2022-08-30 18:12:11 +0100

    get auth user

--- routes/auth.js
+++ routes/auth.js
@@ -5,12 +5,19 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('../config/default.json');
 const { check, validationResult } = require('express-validator');
+const auth = require('../middleware/auth');
 
 // @route   GET api/auth
 // @desc    Get logged in user
 // @access  Private
-router.get('/', (req, res) => {
-	res.send('Get logged in user');
+router.get('/', auth, async (req, res) => {
+	try {
+		const user = await User.findById(req.user.id).select('-password');
+		res.json(user);
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).send('Server Error');
+	}
 });
 
 // @route   POST api/auth
